70ade659c4b4dc6bec160635d3775f0ed760b064
Henry Jiang <h243jian@uwaterloo.ca>
2024-03-08 14:14:21 -0800

    reorganize layouts

--- editor/src/routes/+error.svelte
+++ editor/src/routes/+error.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+    import {page} from '$app/stores';
+</script>
+
 <div class="flex justify-center items-center w-full h-screen">
     <div>
-        <h1 class="text-4xl font-bold">An error has occurred</h1>
+        <h1 class="text-4xl font-bold">{$page.error?.message}</h1>
     </div>
 </div>
--- editor/src/routes/+layout.svelte
+++ editor/src/routes/+layout.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
     import './styles.css';
-    import Navbar from "$lib/layout/Navbar.svelte";
-    import {Separator} from "$lib/components/ui/separator";
-    import HomeSidebar from "$lib/layout/HomeSidebar.svelte";
 </script>
 
-<Navbar/>
-
-<div class="flex-auto flex w-full overflow-y-auto">
-    <slot/>
-</div>
-
+<slot/>
